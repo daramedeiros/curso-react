a1540be45a0071560580b73baf23cb40b701ebd7
Dara Medeiros <daraamedeiros@outlook.com>
2020-03-27 15:14:05 -0300

    atualização do projeto

--- aplicacao-rocketseat/huntweb/README.md
+++
@@ -1,7 +0,0 @@
-
-
-## Huntweb
-
-Amplicação de estudo prático sobre ReactJS. <br />
-
-
--- app-rocketseat/huntweb/src/App.js
+++ app-rocketseat/huntweb/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Rocketseat </h1>
+
     </div>
   );
 }
